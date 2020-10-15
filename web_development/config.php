555a4801f18aab7563d992647da7aaebd0e4db7a
Jeffreytht <tanhoetheng@gmail.com>
2020-10-15 17:08:28 +0800

    Deploy on server

--- web_development/config.php
+++ web_development/config.php
@@ -6,10 +6,10 @@ define('HTTP_SERVER', 'http://localhost/');
 define('HTTPS_SERVER', 'http://localhost/');
 
 // DIR
-define('DIR_APPLICATION', '/home/jeffreytht/Desktop/RealWorldProject/RealWorldProject/web_development/catalog/');
-define('DIR_SYSTEM', '/home/jeffreytht/Desktop/RealWorldProject/RealWorldProject/web_development/system/');
-define('DIR_IMAGE', '/home/jeffreytht/Desktop/RealWorldProject/RealWorldProject/web_development/image/');
-define('DIR_STORAGE', '/home/jeffreytht/Desktop/RealWorldProject/RealWorldProject/public_html/');
+define('DIR_APPLICATION', '/home/tanhoetheng/Desktop/git_repository/ RealWorldProject/web_development/catalog/');
+define('DIR_SYSTEM', '/home/tanhoetheng/Desktop/git_repository/ RealWorldProject/web_development/system/');
+define('DIR_IMAGE', '/home/tanhoetheng/Desktop/git_repository/ RealWorldProject/web_development/image/');
+define('DIR_STORAGE', '/home/tanhoetheng/Desktop/git_repository/ RealWorldProject/public_html/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
