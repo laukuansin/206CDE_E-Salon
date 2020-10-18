f511f061f90f0638c78744669fe8918091e5d4a5
Jeffreytht <tanhoetheng@gmail.com>
2020-10-19 01:33:02 +0800

    Deploy server

--- web_development/config.php
+++ web_development/config.php
@@ -2,20 +2,20 @@
 
 //define('HTTP_SERVER', 'http://35.240.241.182/');
 // define('HTTP_SERVER', 'http://192.168.68.107/');
- define('HTTP_SERVER', 'http://192.168.68.107/');
+ define('HTTP_SERVER', 'http://35.240.241.182');
 
 
 // HTTPS
 //define('HTTPS_SERVER', 'http://35.240.241.182/');
 // define('HTTPS_SERVER', 'http://192.168.68.107/');
- define('HTTPS_SERVER', 'http://192.168.68.107/');
+ define('HTTPS_SERVER', 'http://35.240.241.182/');
 
 
 // DIR
-define('DIR_APPLICATION', '/home/jeffreytht/Desktop/RealWorldProject/RealWorldProject/web_development/catalog/');
-define('DIR_SYSTEM', '/home/jeffreytht/Desktop/RealWorldProject/RealWorldProject/web_development/system/');
-define('DIR_IMAGE', '/home/jeffreytht/Desktop/RealWorldProject/RealWorldProject/web_development/image/');
-define('DIR_STORAGE', '/home/jeffreytht/Desktop/RealWorldProject/RealWorldProject/public_html/');
+define('DIR_APPLICATION', '/home/tanhoetheng/Desktop/git_repository/RealWorldProject/web_development/catalog/');
+define('DIR_SYSTEM', '/home/tanhoetheng/Desktop/git_repository/RealWorldProject/web_development/system/');
+define('DIR_IMAGE', '/home/tanhoetheng/Desktop/git_repository/RealWorldProject/web_development/image/');
+define('DIR_STORAGE', '/home/tanhoetheng/Desktop/git_repository/RealWorldProject/public_html/');
 
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
